e10ed92f1490977722aa0256627ac7a87b4679b9
boutbitnz <boutbitnz2@gmail.com>
2018-01-21 20:20:41 -0600

    Initial Directory Enty

---
+++ PredestineProj/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgModel } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
